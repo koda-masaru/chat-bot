3deb2d7ad565689b4c279bc38c27358dfd45481a
koda <koda.masaru3@gmail.com>
2023-10-01 10:24:01 +0900

    Changing the logic of the standby process.

--- packages/chatbot-backend/src/logic/openai.ts
+++ packages/chatbot-backend/src/logic/openai.ts
@@ -1,24 +1,29 @@
 import { OpenAI } from 'openai'
 
 export async function chat(chat_message: string, apiKey: string) {
-  const start = new Date().getTime()
-  console.log(`start call OpenAI.`)
-  let answer = ''
-  if (chat_message.length > 5) {
-    console.log(`Request to OpenAI API. ${chat_message}`)
-    const openai = new OpenAI({
-      apiKey,
-    })
-    const response = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
-      temperature: 0.7,
-      messages: [{ role: 'user', content: chat_message }],
-    })
-    answer = response.choices[0].message?.content ?? ''
-  } else {
-    answer = '質問は5文字以上で入力してください'
+  try {
+    const start = new Date().getTime()
+    console.log(`start call OpenAI.`)
+    let answer = ''
+    if (chat_message.length > 5) {
+      console.log(`Request to OpenAI API. ${chat_message}`)
+      const openai = new OpenAI({
+        apiKey,
+      })
+      const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        temperature: 0.7,
+        messages: [{ role: 'user', content: chat_message }],
+      })
+      answer = response.choices[0].message?.content ?? ''
+    } else {
+      answer = '質問は5文字以上で入力してください'
+    }
+    console.log(`Result from OpenAI API. ${answer}`)
+    console.log(`finish call OpenAI. ${new Date().getTime() - start}[ms]`)
+    return answer
+  } catch (e) {
+    console.error(e)
+    return 'AIが回答を見つけられませんでした。'
   }
-  console.log(`Result from OpenAI API. ${answer}`)
-  console.log(`finish call OpenAI. ${new Date().getTime() - start}[ms]`)
-  return answer
 }
